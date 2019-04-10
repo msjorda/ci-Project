3c8d86bb80b6d34e5c436c3500538a0ac39b6b04
msjorda <49462985+msjorda@users.noreply.github.com>
2019-04-09 21:35:14 -0400

    UniTORCS v0.1
    Initial UniTORCS framework, car follows waypoints

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Transform frontwheel;
+    public Transform backwheel;
+    public float vel;                           //Note: public variables can be accessed in the side panel of Unity, and also changed on the fly while the game is running
+    public static Rigidbody car;                //Note: static is required for this variable and its components to be accessed in other scripts
+    public Quaternion rot;
+    public static Vector3 direction;
+
+    
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        car = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        direction = frontwheel.position - backwheel.position; //easiest way i could find to calculate a vector for which way the car is facing
+       
+        rot = car.rotation;
+        car.transform.Rotate(0.0f, moveHorizontal, 0.0f, Space.Self);       //rotate car with arrow keys
+        car.AddForce(direction * vel * moveVertical);                       //move car with arrow keys  
+    }  
+}
---
+++ Assets/Scripts/WaypointAI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WaypointAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform[] path;                    //Array where all waypoint objects are stored
+    public Vector3 currentDest;                 //vector from car to current target waypoint
+    
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator FollowPath()
+    {
+        while (true)// runs around waypoints indefinitely
+        {
+            foreach (Transform waypoint in path)
+            {
+                yield return StartCoroutine(Move(waypoint.position, 5));
+            }
+        }
+    }
+
+    IEnumerator Move(Vector3 destination, float speed)
+    {
+        while (Mathf.Abs(transform.position.x - destination.x)> 0.0001f && Mathf.Abs(transform.position.z - destination.z)>0.0001f)  //only checks X and Z coords. height is irrelevant. float value indicates how close it needs to be to waypoint for it to register
+        {
+            currentDest = destination - transform.position; //creates vector pointing from car to next waypoint
+
+            StartCoroutine(RotateFollower(currentDest));
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);      // needs to be replaced with the lerp and slerp functions
+            //CarController.car.AddForce(CarController.direction.normalized * 9);       //currently unused method of moving the car, needs more tuning
+            yield return null;
+        }
+    }
+
+    IEnumerator RotateFollower(Vector3 currentDest)
+    {
+        Vector3 crossProd = Vector3.Cross(CarController.direction, this.currentDest).normalized; //finds crossproduct of the two vectors: (car to next waypoint) and (direction car is facing)
+        print(crossProd);                                                                        //crossproduct is used to determine the direction the car should be turning
+        transform.Rotate(0.0f, crossProd.y, 0.0f, Space.Self);                                   //rotates car towards waypoint     Note: car rotates on an axis relative to itself, not globally
+        yield return null;
+    }
+
+
+
+
+
+}
